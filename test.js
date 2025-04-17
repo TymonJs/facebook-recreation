4cf7cc02acd8d9ae1cce3e240f77cabdd4d5075a
TymonJs <156890105+TymonJs@users.noreply.github.com>
2025-04-17 11:47:38 +0200

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-`curl -X DELETE http://localhost:3000/api/chat -H "Content-type:application/json" -d '{"user1":"kamien","user2":"asd"}'`
-
-console.log("fsodhf fhuosa foisdd fff, @asd. abecadło z pieca spadło"
-);
-
-console.log("abece".substring(1))
